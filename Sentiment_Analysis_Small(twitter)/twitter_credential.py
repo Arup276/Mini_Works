40df0b2dca388a6db9cfd405cb8519bc49661306
Arup Das <32811517+Arup276@users.noreply.github.com>
2019-01-30 23:56:46 +0530

    Update twitter_credential.py

--- Sentiment_Analysis_Small(twitter)/twitter_credential.py
+++ Sentiment_Analysis_Small(twitter)/twitter_credential.py
@@ -1,6 +1,6 @@
 # Variables that contain the user credential to access the twitter API.
-Access_token = '3230808884-wVTH8ZFY9yeK5AA3in47z1KynWDLlOeNdg17ZDP'
-Access_token_secret = 'QgPyN5o3yFoGGtjX6CQbcRCvCiCzMjEdTt9Jv3T0wAoVG'
-Consumer_key = 'JRPQzwJyWJ6p3tc59okIRv4Zd'
-Consumer_secret_key = '21TJ1y1hgwFqD1LobQ8opay3ItELBeiMTtvIyLTblmBDF7EAKO'
+Access_token = 'Enter your Access token'
+Access_token_secret = 'Enter your Access token secret'
+Consumer_key = 'Enter Consumer key'
+Consumer_secret_key = 'Enter Consumer secret key'
 
