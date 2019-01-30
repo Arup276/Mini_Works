218a0bc6bdfb1f7e4caccf9ff3f214aa9e2ec57f
Arup Das <32811517+Arup276@users.noreply.github.com>
2019-01-30 23:51:34 +0530

    Update brain_body.py

--- Brain_Body_Weight_Predict/brain_body.py
+++ Brain_Body_Weight_Predict/brain_body.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[111]:
-
 
 # Dependencies
 import matplotlib.pyplot as plt
@@ -11,23 +9,17 @@ import pandas as pd
 get_ipython().run_line_magic('matplotlib', 'inline')
 
 
-# In[112]:
-
 
 # read data 
 data = pd.read_csv('brai.csv')
 data
 
 
-# In[113]:
-
 
 # selecting data
 input_data = data[['Brain']]
 target_data = data[['Body']]
 
-
-# In[114]:
 
 
 plt.scatter(input_data,target_data,marker='*',color = 'blue')
@@ -36,7 +28,7 @@ plt.ylabel('Body-->')
 plt.show()
 
 
-# In[115]:
+
 
 
 # Creating the object of Linear Regration model and fit or train the model
@@ -46,29 +38,27 @@ model.fit(input_data,target_data)
 
 # ### Now if you want to check the result then use Y=m*x + c formula [m = slop(or gradient) , x = independent variable and c = intercept]
 
-# In[116]:
+
 
 
 # to get the intercept
 model.intercept_
 
 
-# In[117]:
+
 
 
 # to get the slop
 model.coef_
 
 
-# In[118]:
+
 
 
 # Y=     m    *  x  +        c
 Y = 0.96649637*0.300+91.00439620740687
 Y
 
-
-# In[119]:
 
 
 # to see the linear line
@@ -77,9 +67,6 @@ plt.plot(input_data,model.predict(input_data),marker = '*',color = 'blue')
 plt.xlabel('Brain-->')
 plt.ylabel('Body-->')
 plt.show()
-
-
-# In[ ]:
 
 
 
